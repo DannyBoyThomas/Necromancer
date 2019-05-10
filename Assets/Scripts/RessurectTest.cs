2f15fc35afde1df35bf5983adf26454613690ff6
Danny Thomas <daniel1_es@hotmail.com>
2019-05-10 16:55:06 +1000

    Hey, basic resurrection

---
+++ Assets/Scripts/RessurectTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RessurectTest : MonoBehaviour
+{
+    public Transform[] Targets;
+    public TargetProjectile projectile;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            foreach(Transform t in Targets)
+            {
+                var g = GameObject.Instantiate(projectile);
+                g.transform.position = transform.position;
+                g.GetComponent<TargetProjectile>().target = t;
+                g.GetComponent<TargetProjectile>().OnTargetHit += OnTargetHit;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            foreach (Transform t in Targets)
+            {
+                var g = GameObject.Instantiate(projectile);
+                g.transform.position = transform.position;
+                g.GetComponent<TargetProjectile>().target = t;
+                g.GetComponent<TargetProjectile>().OnTargetHit += OnTargetHit2;
+                g.GetComponent<TargetProjectile>().Direction = true;
+            }
+        }
+    }
+
+    void OnTargetHit(GameObject g)
+    {
+        g.GetComponent<Animator>().SetTrigger("Ressurect");
+    }
+    
+      void OnTargetHit2(GameObject g)
+    {
+        g.GetComponent<Animator>().SetTrigger("Extract");
+    }
+}
---
+++ Assets/Scripts/TargetProjectile.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetProjectile : MonoBehaviour
+{
+    public AnimationCurve yOffset;
+    public AnimationCurve lerpSmoothing;
+
+    public Transform target;
+    public Vector3 origin;
+    public float Speed = 5;
+    public float Lerp = 0f;
+    bool delayed = false;
+    public bool Direction = false;
+    public AudioSource CollisionSoundEffect;
+    public AudioSource TravelSoundEffect;
+
+
+    public Action<GameObject> OnTargetHit;
+
+    void Start()
+    {
+        origin = transform.position;
+        if (!Direction)
+            StartCoroutine(DoTarget());
+        else
+            StartCoroutine(DoTargetBackwards());
+    }
+
+    IEnumerator DoTarget()
+    {
+        while (Lerp < 1)
+        {
+            Debug.Log("Do Target");
+            Lerp += Speed * Time.deltaTime;
+            Vector3 pos = Vector3.Lerp(origin, target.position, lerpSmoothing.Evaluate(Lerp));
+            pos += Vector3.up * yOffset.Evaluate(Lerp);
+            transform.position = pos;
+            yield return new WaitForEndOfFrame();
+        }
+        OnTargetHit(target.gameObject);
+        CollisionSoundEffect.pitch = UnityEngine.Random.Range(0.8f, 1.2f);
+        CollisionSoundEffect.Play((ulong)UnityEngine.Random.Range(0f, 0.1f));
+        TravelSoundEffect.Stop();
+    }
+
+    IEnumerator DoTargetBackwards()
+    {
+        Lerp = 1; OnTargetHit(target.gameObject);
+
+        CollisionSoundEffect.pitch = UnityEngine.Random.Range(0.8f, 1.2f);
+        CollisionSoundEffect.Play((ulong)UnityEngine.Random.Range(0f, 0.1f));
+
+
+        while (Lerp > 0)
+        {
+            Debug.Log("Do Target");
+            Lerp -= Speed * Time.deltaTime;
+            Vector3 pos = Vector3.Lerp(origin, target.position, lerpSmoothing.Evaluate(Lerp));
+            pos += Vector3.up * yOffset.Evaluate(Lerp);
+            transform.position = pos;
+            yield return new WaitForEndOfFrame();
+        }
+        TravelSoundEffect.Stop();
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        Destroy(this.gameObject);
+    }
+
+    
+}
